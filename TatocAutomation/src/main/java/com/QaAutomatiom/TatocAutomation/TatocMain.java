cf686f8347cfdd29b8d4c134aefad2f626016cdc
KRISHNA KUAMR <krk5512@gmail.com>
2018-06-25 13:35:57 +0530

    Add files via upload
    Fix:
    " no such element: Unable to locate element" : If this type error encountered then add priority to each test in the test class so that the compiler can analyze the the sequence in which it should execute the test cases.

--- TatocAutomation/src/main/java/com/QaAutomatiom/TatocAutomation/TatocMain.java
+++ TatocAutomation/src/main/java/com/QaAutomatiom/TatocAutomation/TatocMain.java
@@ -1,8 +1,15 @@
 package com.QaAutomatiom.TatocAutomation;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TatocMain {
 
@@ -15,14 +22,16 @@ public class TatocMain {
   public TatocMain(WebDriver driver)
   {
 	   System.setProperty("webdriver.chrome.driver","C:\\Users\\krk55\\Desktop\\chrome\\chromedriver.exe");
-	  this.driver= driver;
+	   this.driver= driver;
   }
   public void checkColorBox(){
     driver.findElement(By.className("greenbox")).click();
   }
   public Boolean checkRepaintBoxWorking()
   {
-	  driver.switchTo().frame("main");
+	  
+	  WebElement pFrame= driver.findElement(By.id("main"));
+	driver.switchTo().frame(pFrame);
 	  oldColor = driver.findElement(By.id("answer")).getAttribute("class");
 	  while(condition) {
 	  driver.switchTo().frame("child");
@@ -40,4 +49,41 @@ public class TatocMain {
 	  return true;
 
   }
+  
+  public Boolean dragTest()
+  {
+      Actions builder = new Actions(driver);	       
+      WebElement from = driver.findElement(By.id("dragbox"));
+      WebElement to = driver.findElement(By.id("dropbox"));
+      Action dragAndDrop = builder.clickAndHold(from).moveToElement(to).release(to).build();
+	   dragAndDrop.perform();
+	   driver.findElement(By.linkText("Proceed")).click();
+	   return true;
+  }
+  
+  public Boolean windowPopTest()
+  {
+	  String parentHandle = driver.getWindowHandle(); // get the current window handle
+      driver.findElement(By.linkText("Launch Popup Window")).click();
+      for (String winHandle : driver.getWindowHandles()) { 
+          driver.switchTo().window(winHandle);        // switch focus of WebDriver to the next found window handle (that's your newly opened window)              
+      }
+      driver.findElement(By.id("name")).sendKeys("Krishna");
+      driver.findElement(By.id("submit")).click();	     
+      driver.switchTo().window(parentHandle);         // switch back to the original window
+	  driver.findElement(By.linkText("Proceed")).click();
+	  return true;
+  }
+public boolean cookieCreateTest() {
+	// TODO Auto-generated method stub
+	driver.findElement(By.linkText("Generate Token")).click();
+    String token = driver.findElement(By.id("token")).getText();
+    System.out.println(token);
+    String[] splitStr = token.split("\\s+");
+    System.out.println(splitStr[1]);
+    Cookie name = new Cookie("Token",splitStr[1]);
+    driver.manage().addCookie(name);
+    driver.findElement(By.linkText("Proceed")).click();	
+	return true;
+}
 }
--- TatocAutomation/src/test/java/com/QaAutomatiom/TatocAutomation/TatocMainTest.java
+++ TatocAutomation/src/test/java/com/QaAutomatiom/TatocAutomation/TatocMainTest.java
@@ -14,20 +14,38 @@ public class TatocMainTest {
 	
 	TatocMain tObject;
 	
-	 @Test
+	 @Test(priority= 0)
 	  public void launchBrowser() {
 		tObject.checkColorBox();  
 	  }
 	 
-	 @Test
+	 @Test(priority= 1)
 	 public void CheckBoxColor()
 	 {
 		 Assert.assertTrue(tObject.checkRepaintBoxWorking());
 	 }
 	
-  @BeforeClass
+	 @Test (priority = 2)
+     public void dragDropTest()
+     {
+		 Assert.assertTrue(tObject.dragTest());
+     }
+	 
+	 
+	 @Test (priority = 3)
+	 public void windowPopUpTest()
+	 {
+		 Assert.assertTrue(tObject.windowPopTest());
+	 }
+	 
+	 @Test (priority = 4)
+	 public void cookieTest() {
+		 Assert.assertTrue(tObject.cookieCreateTest());
+	 }	 
+	 
+	 @BeforeClass
   public void beforeClass() {
-	   System.setProperty("webdriver.chrome.driver","C:\\Users\\krk55\\Desktop\\chrome\\chromedriver.exe");
+	   System.setProperty("webdriver.chrome.driver","C:\\Users\\prkrishnakumar\\eclipse-workspace\\chromedriver.exe");
 	  driver = new ChromeDriver();
       driver.get("http://10.0.1.86/tatoc/basic/grid/gate");
       tObject = new TatocMain(driver);
@@ -35,7 +53,7 @@ public class TatocMainTest {
 
   @AfterClass
   public void afterClass() {
-	  driver.quit();
+	 driver.quit();
   }
 
 }
